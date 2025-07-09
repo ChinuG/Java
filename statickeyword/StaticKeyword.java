357062dea208346e434141bc6caa6253e3e07a9d
Chinmay Gaikwad <97344345+ChinuG@users.noreply.github.com>
2025-07-09 13:36:27 +0530

    Static Keyword
    File added successfully

---
+++ statickeyword/StaticKeyword.java
@@ -0,0 +1,27 @@
+package statickeyword;
+
+public class StaticKeyword {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("From Main method");
+	}
+	static
+	{
+		System.out.println("From static keyword");
+	}
+	
+//	Static Keyword
+//		- Static initializers are the java members which gets executed during the loading process of a class.
+//		- They get executed before the execution of main method.
+
+//	Static Initializers are 2 types
+//		- 1) static declare and initialization statement.
+//		- 2) Static block
+	
+//	1) static declare and initialization statement.
+//		- Here we declare the static variable  and store the values simultaneously.
+	
+//	* Syntax - [access modifier] static datatype variable_name = value/exp
+	
+}
