c0a365b7457019470d089e6b498122d8bb49b713
Chinmay Gaikwad <97344345+ChinuG@users.noreply.github.com>
2025-07-22 23:40:19 +0530

    Number Problems
    Single file factorial of a number is addes successfully

---
+++ NumberProblem/Factorial.java
@@ -0,0 +1,14 @@
+class Factorial 
+{
+	public static void main(String[] args) 
+	{
+		int num = 5;
+		int fact = 1;
+		while(num != 0)
+		{
+			fact  = fact * num;
+			num--;
+		}
+		System.out.println(fact);
+	}
+}
