2d76a8088d74e9751e23b2a2f810875785a146fe
Chinmay Gaikwad <97344345+ChinuG@users.noreply.github.com>
2025-08-10 22:59:07 +0530

    Merge Sort
    example need to check again create method

---
+++ Arrays/Sorting/MergeSort.java
@@ -0,0 +1,38 @@
+package Sorting;
+
+public class MergeSort {
+	public static void main(String args[])
+	{
+		int a[] = {3, 8, 16, 2, 53, 98, 117, 250};
+		int b[] = {0, 5, 6, 11, 18, 25, 70, 83, 99, 190, 280, 300};
+		
+		int ans[] = new int[a.length + b.length];
+		
+		int i=0; 
+		int j=0; 
+		int k=0;
+		while(i<a.length && j<b.length)
+		{
+			if(a[i] > b[j])
+			{
+				ans[k] = b[j];
+				k++;
+				j++;
+			}
+			else if(a[i] <= b[j])
+			{
+				ans[k] = a[i];
+				k++;
+				i++;
+			}
+		}
+		while(i<a.length)
+		{
+			ans[k++] = a[i++];
+		}
+		while(j<b.length)
+		{
+			ans[k++] = b[j++];
+		}
+	}
+}
