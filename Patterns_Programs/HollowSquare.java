bf7a6254d1e3288f07c9077a9b23a31c4d3e8207
Chinmay Gaikwad <97344345+ChinuG@users.noreply.github.com>
2025-08-21 00:08:57 +0530

    Update HollowSquare.java
    Printed Hollow Square

--- Patterns_Programs/HollowSquare.java
+++ Patterns_Programs/HollowSquare.java
@@ -1,24 +1,27 @@
 package patterns;
-
+// Print Hollow Square
+// 		 * * * *
+// 		 *     *
+// 		 *     *
+// 		 *     *
+// 		 * * * *
 public class HollowSquare {
 	public static void main(String args[])
 	{
 		int n = 4;
-//		int arr[][] = {{1, 2, 3}, 
-//				   	   {7, 8, 9}, 
-//				   	   {4,5,6}};
+
 		for(int i=0 ; i<n ; i++)
 		{
 			for(int j=0 ; j<n ; j++)
 			{
 				if(i==0 || i==n-1 || j==0 || j==n-1)
 				{
-					System.out.print("*" + " ");
+					System.out.print("*");
 				}
-//				else
-//				{
-//					System.out.println();
-//				}
+				else
+				{
+					System.out.print(" ");
+				}
 			}
 			System.out.println();
 		}
