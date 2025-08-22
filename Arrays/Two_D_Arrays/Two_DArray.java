5618568b5a38ca1c936d88a875def70d89b91ac2
Chinmay Gaikwad <97344345+ChinuG@users.noreply.github.com>
2025-08-22 20:48:38 +0530

    Update Two_DArray.java
    Changes done successfully

--- Arrays/Two_D_Arrays/Two_DArray.java
+++ Arrays/Two_D_Arrays/Two_DArray.java
@@ -7,15 +7,12 @@ public class Two_DArray {
 					   {4, 5, 6}};
 		int sum = 0;
 		for(int i=0 ; i<arr.length ; i++)
-		{
-			
+		{			
 			for(int j=0 ; j<arr[i].length ; j++)
 			{
 				sum += arr[i][j];
 			}
-			System.out.println(sum);
 		}
 		System.out.println(sum);
-			
 	}
 }
