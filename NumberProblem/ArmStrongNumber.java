57e439c985d8f1c21833bef982faaf7b9ff79aed
Chinmay Gaikwad <97344345+ChinuG@users.noreply.github.com>
2025-09-17 11:02:44 +0530

    Armstrong Number

---
+++ NumberProblem/ArmStrongNumber.java
@@ -0,0 +1,29 @@
+package armstrongnumber;
+
+public class ArmStrongNumber {
+	public static void isArmstrong(int n)
+	{
+		// 153
+		int temp = n;
+		int store = 0;
+		while(n > 0)
+		{
+			int last = n % 10;
+			store += last * last * last;
+			n /= 10;
+		}
+		
+		if(temp == store)
+		{
+			System.out.println(temp + " is Armstrong number");
+		}
+		else
+		{
+			System.out.println(temp + " is NOT an Armstrong number");
+		}
+	}
+	public static void main(String args[])
+	{
+		isArmstrong(153);
+	}
+}
