4400b8a1f72b58b1040907ebce78539ff2fe9369
Chinmay Gaikwad <97344345+ChinuG@users.noreply.github.com>
2025-10-01 22:30:14 +0530

    Static Block example

---
+++ statickeyword/StaticBlock.java
@@ -0,0 +1,20 @@
+package statickeyword;
+
+public class StaticBlock {
+
+	static {
+		System.out.println("Hello World!");
+	}
+
+	public static void main(String args[]) {
+
+	}
+}
+
+// WHy we need main method to run static block
+// ---> - When you run a Java program from command line (java StaticBlock),
+// JVM looks for the main method because that’s the program’s entry point.
+// - While preparing to run main, JVM first loads the class, and at that moment,
+// the static block executes.
+// - Without main, the program has no "entry point". The JVM won’t know what to
+// do after running the static block, so we get an error.
