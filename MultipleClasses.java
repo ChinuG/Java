e5f406a8099e16583418bdffc26a9cb70ba2bd40
Chinmay Gaikwad <97344345+ChinuG@users.noreply.github.com>
2025-10-04 23:28:20 +0530

    Update MultipleClasses.java

--- MultipleClasses.java
+++ MultipleClasses.java
@@ -1,29 +1,32 @@
-package java;
+package Java_Practice;
 
 public class MultipleClasses 
 {
 	public static void main(String[] args) 
 	{
 		System.out.println("Hello World!");
+		First.main();
+		Second.main();
+		Third.main();
 	}
 }
 class First
 {
-	static
+	public static void main()
 	{
 		System.out.println("From First class");
 	}
 }
 class Second
 {
-	static
+	public static void main()
 	{
 		System.out.println("From Second class");
 	}
 }
 class Third
 {
-	static 
+	public static void main()
 	{
 		System.out.println("From Third class");
 	}
