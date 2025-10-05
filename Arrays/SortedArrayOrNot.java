b5836ac67ba738414ccef3cee8654678ce75dc76
Chinmay Gaikwad <97344345+ChinuG@users.noreply.github.com>
2025-10-05 22:51:15 +0530

    Array is sorted or not
    example of array

---
+++ Arrays/SortedArrayOrNot.java
@@ -0,0 +1,30 @@
+package Arrays;
+
+public class SortedArrayOrNot {
+	public static void sortedArray(int ar[])
+	{
+		boolean isSorted = true;
+		for(int i=0 ; i<ar.length-1 ; i++)
+		{
+			if(ar[i] > ar[i+1])
+			{
+				isSorted = false;
+				break;
+			}
+		}
+		if(isSorted)
+		{
+			System.out.println("sorted");
+		}
+		else
+		{			
+			System.out.println("Not sorted");			
+		}
+	}
+	public static void main(String args[])
+	{
+//		int ar[] = {1, 2, 3, 4, 5, 6};
+		int ar[] = {21, 2, 3, 4, 6, 5};
+		sortedArray(ar);
+	}
+}
