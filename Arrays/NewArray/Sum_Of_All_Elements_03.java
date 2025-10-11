63a590dbb5cd4b4ce6aa249566bd7c0594c9a004
Chinmay Gaikwad <97344345+ChinuG@users.noreply.github.com>
2025-10-11 20:04:40 +0530

    Sum of All Elements
    Write a program to print sum of all elements of an Array.

---
+++ Arrays/NewArray/Sum_Of_All_Elements_03.java
@@ -0,0 +1,19 @@
+package NewArrays;
+// Write a program to print sum of all elements of an Array.
+public class Sum_Of_All_Elements_03 
+{
+	public static void sumOfElements(int ar[])
+	{
+		int sum = 0;
+		for(int i=0 ; i<ar.length ; i++)
+		{
+			sum += ar[i];
+		}
+		System.out.println(sum);
+	}
+	public static void main(String gargs[]) 
+	{
+		int ar[] = {1,2,3,4,5,6,7,8,9,10};
+		sumOfElements(ar);
+	}
+}
