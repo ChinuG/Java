7b988eacd791a261a3c355744c77859d67619df1
Chinmay Gaikwad <97344345+ChinuG@users.noreply.github.com>
2025-10-12 22:46:37 +0530

    Copy Array into duplicate array
    Wrate a program to copy array into duplicate Array.

---
+++ Arrays/NewArray/Copy_Array_Into_Duplicate_Array.java
@@ -0,0 +1,24 @@
+package NewArrays;
+// Write a program to copy array into duplicate array.
+
+public class Copy_Array_Into_Duplicate_Array 
+{
+	public static void copyArray(int ar[])
+	{
+		int copyAr[] = new int [ar.length];
+	
+		for(int i=0 ; i<ar.length ; i++)
+		{
+			copyAr[i] = ar[i]; 
+		}
+		for(int i=0 ; i<ar.length ; i++)
+		{
+			System.out.print(copyAr[i] + " ");
+		}
+	}
+	public static void main(String args[])
+	{
+		int ar[] = {3, 34, 6, 4, 3, 6, 7, 4, 3};
+		copyArray(ar);
+	}
+}
