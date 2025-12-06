177db9acffc3ae146ec787e091aedd0db97e48d2
Chinmay Gaikwad <97344345+ChinuG@users.noreply.github.com>
2025-12-06 22:20:53 +0530

    Constructor

---
+++ Constructor/src/constructor/Constructor.java
@@ -0,0 +1,28 @@
+package constructor;
+
+public class Constructor 
+{
+
+	String conName;
+
+	public Constructor()
+	{
+		System.out.println("This is form Constructor 1");
+	}
+	
+	Constructor(String name, int rollNo, int age)
+	{
+		this(name, rollNo);
+		this.conName = name;
+		System.out.println(conName + " Constructor");
+		System.out.println(age + " Constructor");
+	}
+	
+	Constructor(String name, int rollNo)
+	{
+		this();
+		System.out.println(name);
+		System.out.println(rollNo);
+		
+	}
+}
---
+++ Constructor/src/constructor/ConstructorDirver.java
@@ -0,0 +1,19 @@
+package constructor;
+
+
+//	Constructor Overloading Comes under the Compile time Polymorphism
+
+public class ConstructorDirver 
+{
+	public static void main(String args[])
+	{
+//		Constructor c = new Constructor ();
+		
+		Constructor c1 = new Constructor("Amit", 23, 24);
+		
+		
+//		Constructor c2 = new Constructor("Amit", 23);
+		
+		
+	}
+}
