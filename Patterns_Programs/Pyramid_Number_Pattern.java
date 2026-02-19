662051ad4faeff3ec410dee6c05dac376abe5a78
Chinmay Gaikwad <97344345+ChinuG@users.noreply.github.com>
2026-02-19 21:39:48 +0530

    Pyramid Pattern
    Take number input from user (i.e. 5) and then print below pattern
    1
    12
    123
    1234
    12345

---
+++ Patterns_Programs/Pyramid_Number_Pattern.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class Pyramid_Number_Pattern {
+    public static void pattern(int n) {
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int num = sc.nextInt();
+        pattern(num);
+    }
+}
