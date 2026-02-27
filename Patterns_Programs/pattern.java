736eb6dc67cc5ebd7c66cde4584f5532793a8749
Chinmay Gaikwad <97344345+ChinuG@users.noreply.github.com>
2026-02-27 23:41:18 +0530

    Hollow pattern java

---
+++ Patterns_Programs/pattern.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+class pattern {
+
+    public static void printPattern(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+
+                if ((i == 1) || (i == n)) {
+                    System.out.print(" * ");
+                } else if ((j == 1) || (j == n)) {
+                    System.out.print(" * ");
+                } else {
+                    System.out.print("   ");
+                }
+
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number : ");
+        int num = sc.nextInt();
+        printPattern(num);
+    }
+}
